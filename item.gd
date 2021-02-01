f20dfde88d14a24c9edd823e33626c3003e37eae
aash29 <aash29>
2021-02-01 11:11:08 +0300

    a

---
+++ item.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var location = -1
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(get_instance_id())
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
